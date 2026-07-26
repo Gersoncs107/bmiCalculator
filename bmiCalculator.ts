8e7764d7a452707ac43ea62b5ad97805cdcf481a
Gersoncs107 <gerson.silva107@gmail.com>
2026-07-25 22:23:18 -0300

    bmiCalculator-update

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,6 +1,6 @@
 const calculateBMI = (height: number, weight: number): string => {
   const heightInMeters = height / 100; // Convert height from cm to meters
-  const bmi = weight / (height * height);
+  const bmi = weight / (heightInMeters * heightInMeters);
   return bmi.toFixed(2);
 };
 
