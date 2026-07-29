576af573f5e26cd5b7a24d09e46b906146dd987b
Gersoncs107 <gerson.silva107@gmail.com>
2026-07-28 22:35:37 -0300

    bmiCalculator-update

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -8,7 +8,7 @@ interface ExerciseResult {
   explanation: string;
 }
 
-const calculateExercises = (dailyExercises: number[], target: number) => {
+const calculateExercises = (dailyExercises: number[], target: number): ExerciseResult => {
   const totalDays = dailyExercises.length;
   const trainingDays = dailyExercises.filter(day => day > 0).length;
   const totalHours = dailyExercises.reduce((sum, hours) => sum + hours, 0);
